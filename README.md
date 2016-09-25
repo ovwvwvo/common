7901a96f0f94788251b4bcb75a4abfdd8fc5920b
ovwvwvo <jiayaguang123@gmail.com>
2016-09-26 00:42:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+1. CircleIndicator https://github.com/THEONE10211024/CircleIndicator
+
+使用 :   circleIndicator.setViewPager(viewPager);
+
+2. SquareView 
+
+3. 
---
+++ src/main/java/com/ovwvwvo/common/widget/SquareView.java
@@ -0,0 +1,38 @@
+package com.ovwvwvo.common.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by guang on 16/8/21.
+ */
+public class SquareView extends View {
+
+    public SquareView(Context context) {
+        super(context);
+    }
+
+    public SquareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
+        int childWidthSize = getMeasuredWidth();
+        heightMeasureSpec = widthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
